7aad01cfa65e846e3499a9460fd612e71b1d922a
Jefferson Hernandez <jeffersonh44@gmail.com>
2017-02-23 16:00:11 -0500

    canny with noise...

--- src/swt.js
+++ src/swt.js
@@ -1,49 +1,111 @@
 "use strict";
 
 var Image = require("image-js");
+const MAX_BRIGHTNESS = 255;
+
+/*function gaussianFilter(image, width, height, sigma) {
+    var n = 2 * Math.floor(2 * sigma) + 3;
+    var mean = Math.floor(n / 2.0);
+    var kernel = new Array(n * n); // variable length array
+
+    //fprintf(stderr, "gaussian_filter: kernel size %d, sigma=%g\n",
+    //    n, sigma);
+    var c = 0;
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < n; j++) {
+            kernel[c] = Math.exp(-0.5 * (Math.pow((i - mean) / sigma, 2.0) +
+                    Math.pow((j - mean) / sigma, 2.0)))
+                / (2 * Math.PI * sigma * sigma);
+            c++;
+        }
+    }
+
+    return convolution(image, kernel, width, height, n, true);
+}
+
+function convolution(image, kernel, width, height, kn, normalize) {
+    var out = new Array(width * height);
+    const khalf = Math.floor(kn / 2);
+    var min = Number.MAX_VALUE, max = -Number.MAX_VALUE;
+
+    if(normalize) {
+        for(var m = khalf; m < width - khalf; m++) {
+            for(var n = khalf; n < height - khalf; n++) {
+                var pixel = 0.0, c = 0;
+                for (var j = -khalf; j <= khalf; j++) {
+                    for (var i = -khalf; i <= khalf; i++) {
+                        pixel += image[(n - j) * width + m - i] * kernel[c];
+                        c++;
+                    }
+                    if (pixel < min) {
+                        min = pixel;
+                    }
+                    if (pixel > max) {
+                        max = pixel;
+                    }
+                }
+            }
+        }
+    }
+
+    for (m = khalf; m < width - khalf; m++) {
+        for (n = khalf; n < height - khalf; n++) {
+            pixel = 0.0;
+            c = 0;
+            for (j = -khalf; j <= khalf; j++) {
+                for (i = -khalf; i <= khalf; i++) {
+                    pixel += image[(n - j) * width + m - i] * kernel[c];
+                    c++;
+                }
+
+                if (normalize)
+                    pixel = MAX_BRIGHTNESS * (pixel - min) / (max - min);
+                out[n * width + m] = pixel;
+            }
+        }
+    }
+
+    return out;
+}*/
 
 function canny(image, options) {
-    options = options | {};
-    var sigma = options.sigma | 0.2;
-    var tMin = options.lowThreshold | 50;
-    var tMax = options.highThreshold | 150;
-    var blur = options.blur | 1;
-    const MAX_BRIGHTNESS = 255;
-
     var width = image.width, height = image.height;
 
-    var Gx = [[-1, 0, 1],
-        [-2, 0, 2],
-        [-1, 0, 1]];
-    var Gy = [[1, 2, 1],
-        [0, 0, 0],
-        [-1, -2, 1]];
+    options = options | {};
+    var tMin = options.lowThreshold | 35;
+    var tMax = options.highThreshold | 75;
+    var blur = options.blur | 1;
+
+    var Gx = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
+    var Gy = [[1, 2, 1], [0, 0, 0], [-1, -2, 1]];
 
     var gf = image.gaussianFilter({
-        sigma: sigma
+        sigma: blur,
+        radius: 3
     });
 
-    var gradientX = image.convolution(Gx);
-    gradientX.save("gradientx.jpg");
-    var gradientY = image.convolution(Gy);
-    gradientY.save("gradienty.jpg");
-    var G = image.sobelFilter({
-        kernelX: Gx,
-        kernelY: Gy
-    });
+    var gradientX = gf.convolution(Gx);
+    var gradientY = gf.convolution(Gy);
+    var G = new Array(width * height);
+
+    for(var i = 1; i < width - 1; i++) {
+        for(var j = 1; j < height - 1; j++) {
+            c = i + width * j;
+            G[c] = Math.floor(Math.hypot(gradientX.getPixel(c)[0], gradientY.getPixel(c)[0]));
+        }
+    }
 
     var nms = new Array(width * height);
     var edges = new Array(width * height);
-    for(var i = 0; i < nms.length; ++i) {
+    for(i = 0; i < nms.length; ++i) {
         nms[i] = 0;
         edges[i] = 0;
     }
 
     var output = new Image(width, height).grey();
-
     // non-maximum supression
     for (i = 1; i < width - 1; i++) {
-        for (var j = 1; j < height - 1; j++) {
+        for (j = 1; j < height - 1; j++) {
             var c = i + width * j;
             var nn = c - width;
             var ss = c + width;
@@ -54,18 +116,17 @@ function canny(image, options) {
             var sw = ss + 1;
             var se = ss - 1;
 
-            var dir = (Math.atan2(gradientY.getPixelXY(i, j)[0] % gradientX.getPixelXY(i, j)[0]
-                    + Math.PI, Math.PI) / Math.PI) * 8;
+            var dir = Math.floor(((Math.atan2(gradientY.getPixel(c)[0], gradientX.getPixel(c)[0]) + Math.PI) % Math.PI) / Math.PI) * 8;
 
-            if (((dir <= 1 || dir > 7) && G.getPixel(c) > G.getPixel(ee) &&
-                G.getPixel(c) > G.getPixel(ww)) || // 0 deg
-                ((dir > 1 && dir <= 3) && G.getPixel(c) > G.getPixel(nw) &&
-                G.getPixel(c) > G.getPixel(se)) || // 45 deg
-                ((dir > 3 && dir <= 5) && G.getPixel(c) > G.getPixel(nn) &&
-                G.getPixel(c) > G.getPixel(ss)) || // 90 deg
-                ((dir > 5 && dir <= 7) && G.getPixel(c) > G.getPixel(ne) &&
-                G.getPixel(c) > G.getPixel(sw)))   // 135 deg
-                nms[c] = G.getPixel(c);
+            if (((dir <= 1 || dir > 7) && G[c] > G[ee] &&
+                G[c] > G[ww]) || // 0 deg
+                ((dir > 1 && dir <= 3) && G[c] > G[nw] &&
+                G[c] > G[se]) || // 45 deg
+                ((dir > 3 && dir <= 5) && G[c] > G[nn] &&
+                G[c] > G[ss]) || // 90 deg
+                ((dir > 5 && dir <= 7) && G[c] > G[ne] &&
+                G[c] > G[sw]))   // 135 deg
+                nms[c] = G[c];
             else
                 nms[c] = 0;
         }
@@ -111,7 +172,6 @@ function canny(image, options) {
 
 Image.load("./img/billboard.jpg").then(function(image) {
     var grey = image.grey();
-    grey = grey.sobelFilter();
     grey = canny(grey);
     grey.save("test.jpg");
 });
